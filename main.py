f855b3536f5f1048507ae2416337c684fa9feec4
Arthur <artursharipv@mail.ru>
2023-02-26 22:37:54 +0500

    change output json file to dict

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from selenium.webdriver.chrome.service import Service
 from fake_useragent import UserAgent
 import time
 import requests
-import datetime
+from datetime import date
 import random
 import re
 import json
+import hashlib
 
 def get_source_html(url):
     #Функция для скачивания html файла с сайта ozon
@@ -28,14 +29,20 @@ def get_source_html(url):
     finally:
         driver.close()
         driver.quit()
+def read_data_file():
+    try:
+        with open(results_json_file, "r", encoding="utf-8") as read_file:
+            return json.load(read_file)
+    except Exception as ex:
+        return {}
 
 def main():
     html_path = f"E:\pyton\OzonParser2\html\ozon_page.html"
-    result_list = []
+    data_dict = read_data_file()
     with open(url_txt_file, "r", encoding="utf-8") as url_file:
         urls = [i.split('?')[0] for i in url_file.readlines()]
     for item_url in urls:
-        request_time = str(datetime.datetime.now())[:-7]
+        request_date = str(date.today())
         get_source_html(item_url)
         try:
             with open(html_path, encoding="utf-8") as file:
@@ -44,18 +51,24 @@ def main():
                 item_price = (''.join(price_text_list)).replace('\u2009', '')
                 name_text_list = re.findall(r"webProductHeading\"><h1[^>]*?>([^<]*?)</h1>", src)
                 item_name = name_text_list[0]
-                result_list.append(
-                    {
-                        "item_name": item_name,
-                        "item_price": item_price,
-                        "item_url": item_url,
-                        "request_time": request_time
+                item_name_and_url = item_name+item_url
+                item_hash_obj = hashlib.md5(item_name_and_url.encode())
+                item_hash = item_hash_obj.hexdigest()
+                if item_hash in data_dict:
+                    if request_date not in data_dict[item_hash]['request_date']:
+                        data_dict[item_hash]['item_price'].append(item_price)
+                        data_dict[item_hash]['request_date'].append(request_date)
+                else:
+                    data_dict[item_hash]={
+                            "item_name": item_name,
+                            "item_url": item_url,
+                            "item_price": [item_price],
+                            "request_date": [request_date]
                     }
-                )
         except Exception as ex:
             print(ex)
-    with open(results_json_file, "a", encoding="utf-8") as file:
-        json.dump(result_list, file, indent=4, ensure_ascii=False)
+    with open(results_json_file, "w", encoding="utf-8") as file:
+        json.dump(data_dict, file, indent=4, ensure_ascii=False)
 
 
 #Абсолютный путь к файлу со списком url на товары
